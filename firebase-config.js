46277d37adf9228cc62aa03485fa0e76f3a6cce7
saurusrawr <kolisedangapa35@gmail.com>
2026-01-02 12:17:47 +0700

    Add Firebase configuration file
    This file contains the Firebase configuration settings for the project.

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js (jangan share file ini!)
+export const firebaseConfig = {
+  apiKey: "AIzaSyBer_DF3-zAyH2rmnMpzilUiE5BYTb4In0",
+  authDomain: "saurusproject-6974b.firebaseapp.com",
+  databaseURL: "https://saurusproject-6974b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "saurusproject-6974b",
+  storageBucket: "saurusproject-6974b.appspot.com",
+  messagingSenderId: "519472343742",
+  appId: "1:519472343742:web:xxxxxxxxxxxxxxxx" // isi kalau ada
+};
